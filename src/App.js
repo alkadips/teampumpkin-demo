a86676291cd1aa8e9a9cb61e5991711e6efb6e7b
Alka <rashinkaralka@gmail.com>
2023-11-10 01:05:22 +0530

    Added navigation by using arrow keys

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useMemo } from "react";
-import { useCallback } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-// import Carousel from "react-material-ui-carousel";
-function App() {
-  const [data, setData] = useState([
+import React, { Component } from "react";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.navigateTitles = this.navigateTitles.bind(this);
+    this.state = {
+      activeTab: this.staticData[0],
+      activeTitle: this.staticData[0].titles[0],
+      data: this.staticData,
+    };
+    this.selectorRef = React.createRef(null);
+  }
+
+  staticData = [
     {
       franchisee: "Call of Duty",
+      index: 0,
       titles: [
         {
           title: "Modern Warefare 1",
+          index: 0,
           cover_image: "https://images8.alphacoders.com/102/1021953.jpg",
           background_image:
             "https://c4.wallpaperflare.com/wallpaper/364/251/812/call-of-duty-4-modern-warfare-remastered-call-of-duty-4-modern-warfare-remastered-call-of-duty-wallpaper-preview.jpg",
@@ -18,6 +27,7 @@ function App() {
         },
         {
           title: "Modern Warefare 2",
+          index: 1,
           cover_image:
             "https://cdn.mos.cms.futurecdn.net/Ute2aEVA8SqD7sGVTzX5pn-970-80.jpg.webp",
           background_image:
@@ -27,6 +37,7 @@ function App() {
         },
         {
           title: "Modern Warefare 3",
+          index: 2,
           cover_image:
             "https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/WP%20Media%20Folder%20-%20esports-com/app/uploads/2021/08/COD-MW3-720x405.jpg",
           background_image:
@@ -38,9 +49,11 @@ function App() {
     },
     {
       franchisee: "Grand Theft Auto",
+      index: 1,
       titles: [
         {
           title: "Grand Theft Auto 1",
+          index: 0,
           cover_image:
             "https://upload.wikimedia.org/wikipedia/en/e/e7/GTA_-_Box_Front.jpg",
           background_image:
@@ -50,6 +63,7 @@ function App() {
         },
         {
           title: "Grand Theft Auto 2",
+          index: 1,
           cover_image:
             "https://psxmedia.ign.com/psx/image/object/012/012390/600209.jpg",
           background_image:
@@ -59,6 +73,7 @@ function App() {
         },
         {
           title: "Grand Theft Auto 3",
+          index: 2,
           cover_image:
             "https://cdn.mobygames.com/covers/4175686-grand-theft-auto-iii-windows-front-cover.jpg",
           background_image: "https://wallpaperaccess.com/full/1443968.jpg",
@@ -69,9 +84,11 @@ function App() {
     },
     {
       franchisee: "Assassin's Creed",
+      index: 2,
       titles: [
         {
           title: "ASSASSIN’S CREED – 2007",
+          index: 0,
           cover_image:
             "https://cdn.mos.cms.futurecdn.net/fKZ9fz38ruD9buwcibP5Ba-320-80.jpg",
           background_image: "https://images5.alphacoders.com/540/540654.jpg",
@@ -80,6 +97,7 @@ function App() {
         },
         {
           title: "Assassin's Creed II (2009)",
+          index: 1,
           cover_image:
             "https://oyster.ignimgs.com/mediawiki/apis.ign.com/assassins-creed-2/e/e9/Assassins_creed_ii.jpg",
           background_image: "https://images.alphacoders.com/109/109613.jpg",
@@ -88,6 +106,7 @@ function App() {
         },
         {
           title: "Assassin's Creed: Brotherhood (2010)",
+          index: 2,
           cover_image:
             "https://m.media-amazon.com/images/M/MV5BZjc5NWU2ZjMtNTVkOC00MzBmLWI5M2YtZmVhNWU5Mjg2ZGQyXkEyXkFqcGdeQXVyMjM5NzU3OTM@._V1_.jpg",
           background_image:
@@ -97,142 +116,174 @@ function App() {
         },
       ],
     },
-  ]);
-  const [activeTab, setActiveTab] = useState(data[0].franchisee);
-  const [activeTitle, setActiveTitle] = useState(data[0].titles[0]);
-  // const [activeChild, setActiveChild] = useState(0);
-  const items = useMemo(() => [1, 2, 3, 4], []);
-  const changeChild = useCallback(
-    (e) => {
+  ];
+  navigateTitles(e) {
+    this.setState((state) => {
+      const { activeTitle, activeTab, data } = state;
+      if (
+        ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].indexOf(e.key) ===
+        -1
+      )
+        return state;
+      this.selectorRef.current.blur();
       if (e.key === "ArrowLeft") {
-        setActiveTab((a) => (a - 1 < 0 ? items.length - 1 : a - 1));
+        if (activeTitle.index === 0) {
+          const newTabIndex =
+            activeTab.index - 1 < 0 ? data.length - 1 : activeTab.index - 1;
+          return {
+            ...state,
+            activeTab: data[newTabIndex],
+            activeTitle:
+              data[newTabIndex].titles[data[newTabIndex].titles.length - 1],
+          };
+        }
+        const newTitleIndex =
+          activeTitle.index - 1 < 0
+            ? data[activeTab.index].titles.length - 1
+            : activeTitle.index - 1;
+        const activeTitledata = data[activeTab.index].titles[newTitleIndex];
+        return { ...state, activeTitle: activeTitledata };
       } else if (e.key === "ArrowRight") {
-        setActiveTab((a) => (a + 1 > items.length - 1 ? 0 : a + 1));
-      } else if (e.key === "ArrowTop") {
-        setActiveTab((a) => (a - 1 > items.length - 1 ? 0 : a - 1));
-      } else if (e.key === "ArrowBottom") {
-        setActiveTab((a) => (a + 1 > items.length - 1 ? 0 : a + 1));
+        if (activeTitle.index + 1 === data[activeTab.index].titles.length) {
+          const newTabIndex =
+            activeTab.index + 1 > data.length - 1 ? 0 : activeTab.index + 1;
+          return {
+            ...state,
+            activeTab: data[newTabIndex],
+            activeTitle: data[newTabIndex].titles[0],
+          };
+        }
+        const newTitleIndex =
+          activeTitle.index + 1 > data[activeTab.index].titles.length - 1
+            ? 0
+            : activeTitle.index + 1;
+        const activeTitledata = data[activeTab.index].titles[newTitleIndex];
+
+        return { ...state, activeTitle: activeTitledata };
+      } else if (e.key === "ArrowUp") {
+        return { ...state, activeTab: data[0], activeTitle: data[0].titles[0] };
+      } else if (e.key === "ArrowDown") {
+        this.selectorRef.current.focus();
       }
-    },
-    [items]
-  );
-
-  useEffect(() => {
-    document.addEventListener("keydown", changeChild);
-
-    return function cleanup() {
-      document.removeEventListener("keydown", changeChild);
-    };
-  });
-  return (
-    <div
-      className="wrapper"
-      style={{
-        backgroundImage: `url(${activeTitle.background_image})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="flex justify-between p-5">
-        <div className="flex gap-5">
-          {data.map((frenchiseData) => {
-            return (
-              <div>
-                <div
-                  style={{
-                    cursor: "pointer",
-                    fontWeight: `${
-                      activeTab == frenchiseData.franchisee ? "bold" : "normal"
-                    }`,
-                    color: `${
-                      activeTab == frenchiseData.franchisee ? "white" : "gray"
-                    }`,
-                  }}
-                  onClick={() => {
-                    setActiveTab(frenchiseData.franchisee);
-                    setActiveTitle(frenchiseData.titles[0]);
-                  }}
-                >
-                  {frenchiseData.franchisee}
+      return state;
+    });
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.navigateTitles);
+  }
+  render() {
+    const { activeTab, activeTitle, data } = this.state;
+    return (
+      <div
+        className="wrapper"
+        style={{
+          backgroundImage: `url(${activeTitle.background_image})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="flex justify-between p-5">
+          <div className="flex gap-5">
+            {data.map((frenchiseData) => {
+              return (
+                <div>
+                  <div
+                    style={{
+                      fontWeight: `${
+                        activeTab.index === frenchiseData.index
+                          ? "bold"
+                          : "normal"
+                      }`,
+                      color: `${
+                        activeTab.index === frenchiseData.index
+                          ? "white"
+                          : "gray"
+                      }`,
+                    }}
+                  >
+                    {frenchiseData.franchisee}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <div className="flex gap-5 text-white">
+            <div>
+              <img className="w-5 h-5" src="search2.png"></img>
+            </div>
+            <div>
+              <img className="w-5 h-5" src="settings2.png"></img>
+            </div>
+            <div>
+              <img className="w-5 h-5" src="profile.png"></img>
+            </div>
+            <div>07:35 PM</div>
+          </div>
         </div>
-        <div className="flex gap-5 text-white">
-          <div>
-            <img
-              style={{ width: "20px", height: "20px" }}
-              src="search2.png"
-            ></img>
-          </div>
-          <div>
-            <img
-              style={{ width: "20px", height: "20px" }}
-              src="settings2.png"
-            ></img>
-          </div>
-          <div>
-            <img
-              style={{ width: "20px", height: "20px" }}
-              src="profile.png"
-            ></img>
-          </div>
-          <div>07:35 PM</div>
-        </div>
-      </div>
 
-      <div className=" mt-5" style={{ padding: "0px 0px 0px 75px" }}>
-        {data
-          .filter((d) => d.franchisee === activeTab)
-          .map((fren) => {
-            return (
-              <div className="flex gap-5 text-red-600">
-                {fren.titles.map((title) => {
-                  return (
-                    <div onClick={() => setActiveTitle(title)}>
-                      <img
-                        style={{
-                          cursor: "pointer",
-                          width: `${
-                            activeTitle.title === title.title ? "145px" : "50px"
-                          }`,
-                          height: `${
-                            activeTitle.title === title.title ? "145px" : "50px"
-                          }`,
-                          borderRadius: "15px",
+        <div className=" mt-5 pl-16 h-36">
+          {data
+            .filter((d) => d.franchisee === activeTab.franchisee)
+            .map((fren) => {
+              return (
+                <div className="flex gap-5 text-red-600">
+                  {fren.titles.map((title) => {
+                    const isActiveElement = activeTitle.title === title.title;
+                    const focusCss = isActiveElement
+                      ? {
+                          width: "145px",
                           border: "2px solid #f688f6",
-                        }}
-                        src={title?.cover_image}
-                      ></img>
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })}
-        <div className="mt-5 text-white title-demo">
-          <span>{activeTab}:</span>
+                          height: "145px",
+                          borderRadius: "15px",
+                        }
+                      : {
+                          width: "50px",
+                          height: "50px",
+                          borderRadius: "15px",
+                          border: "2px solid #fff",
+                        };
+                    return (
+                      <div>
+                        <img
+                          className="test-img"
+                          style={{ ...focusCss }}
+                          src={title?.cover_image}
+                        ></img>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+        </div>
+        <div className="mt-5 ml-5 text-white title-demo">
+          <span>{activeTab.franchisee}:</span>
           <span>{activeTitle.title}</span>
         </div>
-      </div>
+        <div className="ml-5 mt-24">
+          <div className="ml-5 title-fre uppercase ... font-bold">
+            {activeTab.franchisee}
+          </div>
+          <div className="ml-5 title-fren2 uppercase ... font-bold">
+            {activeTitle.title}
+          </div>
+          <div className="mt-3 ">
+            <button ref={this.selectorRef} className="buy-now">
+              Buy now
+            </button>
+          </div>
+        </div>
 
-      <div className="ml-5" style={{ marginTop: "100px" }}>
-        <div className="ml-5 title-fre" style={{ textTransform: "uppercase" }}>
-          {activeTab}
-        </div>
-        <div className="ml-5 title-fren2">{activeTitle.title}</div>
-        <div className="mt-3">
-          <button className="buy-now">Buy now</button>
-        </div>
-      </div>
-
-      <div className="flex justify-end" style={{ marginTop: "100px" }}>
-        <div>
-          <div className=" title-fre3">{activeTab}</div>
-          <div className="title-fren3">{activeTitle.title}</div>
+        <div className="flex justify-end mt-24">
+          <div className="last-column">
+            <div className=" title-fre uppercase ... font-bold">
+              {activeTab.franchisee}
+            </div>
+            <div className="title-fren3 uppercase ... font-bold">
+              {activeTitle.title}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-export default App;
